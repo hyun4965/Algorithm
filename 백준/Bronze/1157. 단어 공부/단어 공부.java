c80d490075c56f3ae485c4644f37f74e14a62097
SeokHyun Bae <50188017+hyun4965@users.noreply.github.com>
2024-04-27 15:24:00 +0900

    [Bronze I] Title: 단어 공부, Time: 272 ms, Memory: 20116 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,38 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
  
 public class Main {
  
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
  
-		int[] arr = new int[26];
-		String s = sc.next();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] arr = new int[26]; // 영문자의 개수는 26개임
+		String s = br.readLine();
+		
  
-		for (int i = 0; i < s.length(); i++){
- 
-			if ('A' <= s.charAt(i) && s.charAt(i) <= 'Z') { 
-				arr[s.charAt(i) - 'A']++;	
-			}
-    
-			else {
-				arr[s.charAt(i) - 'a']++;
+		for (int i = 0; i < s.length(); i++) {
+			if ('a' <= s.charAt(i) && s.charAt(i) <= 'z') {
+				arr[s.charAt(i) - 97]++;
+			} else {
+				arr[s.charAt(i) - 65]++;
 			}
 		}
- 
- 
 		int max = -1;
 		char ch = '?';
- 
 		for (int i = 0; i < 26; i++) {
+ 
 			if (arr[i] > max) {
 				max = arr[i];
-				ch = (char) (i + 65); 
-			}
+				ch = (char) (i + 65);
+			} 
 			else if (arr[i] == max) {
 				ch = '?';
 			}
 		}
- 
 		System.out.print(ch);
 	}
  
