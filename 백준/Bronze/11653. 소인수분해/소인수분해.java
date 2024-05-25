2e03a9679b166cba22bff871b2e7eb6a23cd2b3b
SeokHyun Bae <50188017+hyun4965@users.noreply.github.com>
2024-05-25 14:59:11 +0900

    [Bronze I] Title: 소인수분해, Time: 240 ms, Memory: 17756 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -4,13 +4,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        sc.close();
-
-        for (int i = 2; i <= num; i++) {
-            while (num % i == 0) {
-                System.out.println(i);
-                num /= i;
+        int i = 2;
+        while (true) {
+            if (num == 1) {
+                return;
             }
+            if (num == i) {
+                break;
+            }
+            if (num % i == 0) {
+                System.out.println(i);
+                num = num / i;
+            } else {
+                i++;
+            }
+        }
+        if (num > 1) {
+            System.out.println(num);
         }
     }
 }
