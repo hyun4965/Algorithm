841b5e558206208470385d356358456af5bd0283
SeokHyun Bae <50188017+hyun4965@users.noreply.github.com>
2024-05-26 15:45:43 +0900

    [Bronze III] Title: 대지, Time: 324 ms, Memory: 27768 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine()); // 점의 개수
+
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+        }
+
+        int area = (maxX - minX) * (maxY - minY);
+
+        bw.write(area + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
