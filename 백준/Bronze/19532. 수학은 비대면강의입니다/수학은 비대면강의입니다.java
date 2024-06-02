2b478217123bb2d5a9b557adff07edd0a3ad94e0
SeokHyun Bae <50188017+hyun4965@users.noreply.github.com>
2024-06-02 16:52:39 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 252 ms, Memory: 18456 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+        int x = 0;
+        int y = 0;
+        sc.close();
+        for (int i = -999; i < 1000; i++) {
+            for (int j = -999; j < 1000; j++) {
+                if ((a * i + b * j == c) && (d * i + e * j == f)) {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        System.out.print(x + " " + y);
+    }
+}
