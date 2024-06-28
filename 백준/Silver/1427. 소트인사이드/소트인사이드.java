59143e4274cf25998170abc253deb7a10c66e7c7
SeokHyun Bae <50188017+hyun4965@users.noreply.github.com>
2024-06-28 11:33:27 +0900

    [Silver V] Title: 소트인사이드, Time: 208 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String N = sc.next();
+        
+        char[] digits = N.toCharArray();
+        
+        Arrays.sort(digits);
+        
+        for (int i = digits.length - 1; i >= 0; i--) {
+            System.out.print(digits[i]);
+        }
+    }
+}
