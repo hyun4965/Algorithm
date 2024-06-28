149b84219c860e8049524cbf839e9097d0ebcb28
SeokHyun Bae <50188017+hyun4965@users.noreply.github.com>
2024-06-28 12:15:06 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2164 ms, Memory: 192756 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+class Point {
+    int x, y;
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        List<Point> points = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            points.add(new Point(x, y));
+        }
+
+        Collections.sort(points, new Comparator<Point>() {
+            @Override
+            public int compare(Point p1, Point p2) {
+                if (p1.x != p2.x) {
+                    return Integer.compare(p1.x, p2.x);
+                } else {
+                    return Integer.compare(p1.y, p2.y);
+                }
+            }
+        });
+
+        for (Point p : points) {
+            System.out.println(p.x + " " + p.y);
+        }
+    }
+}
