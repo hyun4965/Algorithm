d1880607e1d06ce9af254c3787053ae15b3db95a
SeokHyun Bae <50188017+hyun4965@users.noreply.github.com>
2024-06-28 11:36:12 +0900

    [Silver V] Title: 소트인사이드, Time: 180 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,17 +1,24 @@
 import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        String N = sc.next();
-        
-        char[] digits = N.toCharArray();
-        
-        Arrays.sort(digits);
-        
-        for (int i = digits.length - 1; i >= 0; i--) {
-            System.out.print(digits[i]);
+        int[] counting = new int[10]; // 각 자리수의 빈도를 저장하는 배열
+        int N = sc.nextInt();
+
+        // 각 자리수의 빈도를 계산
+        while (N != 0) {
+            int digit = N % 10;  // 마지막 자리수
+            counting[digit]++;   // 해당 자리수의 빈도 증가
+            N /= 10;             // 마지막 자리수 제거
+        }
+
+        // 내림차순으로 출력
+        for (int k = 9; k >= 0; k--) {
+            for (int j = 0; j < counting[k]; j++) {
+                System.out.print(k);
+            }
         }
     }
 }
