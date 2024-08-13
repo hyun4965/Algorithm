ed8a10a994f1e1038016d22dfcb5170f13649349
SeokHyun Bae <50188017+hyun4965@users.noreply.github.com>
2024-08-13 19:09:43 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 992 ms, Memory: 65336 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < T; i++) {
+            String s = sc.nextLine();
+            String[] words = s.split(" ");
+
+            StringBuilder result = new StringBuilder();
+            for (String word : words) {
+                StringBuilder reversedWord = new StringBuilder(word).reverse();
+                result.append(reversedWord).append(" ");
+            }
+            System.out.println(result.toString().trim()); 
+        }
+        sc.close();
+    }
+}
