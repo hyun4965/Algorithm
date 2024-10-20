8496dd0b5a7bfd66c6dd55c76c1b0baa5af7d97e
SeokHyun Bae <50188017+hyun4965@users.noreply.github.com>
2024-10-20 14:39:01 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int num) {
+        if (num == 1) return 0;
+
+        int answer = 0;
+        long n = num; 
+
+        while (n != 1) {
+            answer++;
+            
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = n * 3 + 1;
+            }
+
+            if (answer > 500) {
+                return -1;
+            }
+        }
+
+        return answer;
+    }
+}
