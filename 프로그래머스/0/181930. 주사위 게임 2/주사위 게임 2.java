9f499d394146484f3a08569a745bb1846ad5be52
SeokHyun Bae <50188017+hyun4965@users.noreply.github.com>
2024-10-29 15:08:26 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        if(a==b && a== c){
+            answer = (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
+        }else if((a==b && a!=c) || (a==c && a!=b)|| (b==c && b!=a)){
+            answer = (a+b+c)*(a*a+b*b+c*c);
+        }else{
+            answer = a+b+c;
+        }
+        return answer;
+    }
+}
