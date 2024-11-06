6747eff2131147109fb66300ce0664859cce7220
SeokHyun Bae <50188017+hyun4965@users.noreply.github.com>
2024-11-06 17:34:43 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 60.36 ms, Memory: 130 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        char a = '수';
+        char b = '박';
+        for(int i=0; i<n;i++){
+            if(i%2!=1){
+                answer +=a;
+            }else{
+                answer+=b;
+            }
+        }
+        return answer;
+    }
+}
