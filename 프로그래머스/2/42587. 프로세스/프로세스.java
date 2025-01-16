451dcd21c11390001ff19635ecf4ad9094c423fa
SeokHyun Bae <50188017+hyun4965@users.noreply.github.com>
2025-01-16 23:04:22 +0900

    [level 2] Title: 프로세스, Time: 2.02 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(new int[]{priorities[i], i});
+        }
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll(); 
+            boolean hasHigherPriority = false;
+
+            for (int[] process : queue) {
+                if (process[0] > current[0]) {
+                    hasHigherPriority = true;
+                    break;
+                }
+            }
+
+            if (hasHigherPriority) {
+                queue.add(current);
+            } else {
+                answer++;
+                if (current[1] == location) {
+                    return answer;
+                }
+            }
+        }
+        return answer;
+    }
+}
