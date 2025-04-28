52069c53c8dcdc0a44d3c05266d530b094ac2b42
SeokHyun Bae <50188017+hyun4965@users.noreply.github.com>
2025-04-28 10:58:01 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] common) {
+        if (common[1] - common[0] == common[2] - common[1]) {
+            int diff = common[1] - common[0];
+            return common[common.length - 1] + diff;
+        } else {
+            int ratio = common[1] / common[0]; 
+            return common[common.length - 1] * ratio;
+        }
+    }
+}
