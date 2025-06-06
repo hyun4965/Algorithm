9dbbffb24fd2d9b275cba2c6954706fb7927876c
BAE <50188017+hyun4965@users.noreply.github.com>
2025-06-06 11:04:45 +0900

    [level 1] Title: [1차] 다트 게임, Time: 2.15 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int solution(String dartResult) {
+        int answer = 0;
+        int[] dart = new int[3];
+
+        int n=0;
+        int index=0;
+        String numstr="";
+        
+        for(int i=0;i<dartResult.length();i++){
+            char c = dartResult.charAt(i);
+            
+            if(c>='0'&&c<='9'){       
+                 numstr+=String.valueOf(c);
+                
+            } else if(c=='S'||c=='D'||c=='T'){
+                n=Integer.parseInt(numstr);
+               if(c=='S'){
+                   dart[index++]=(int)Math.pow(n,1);
+                 } else if(c=='D'){
+                    dart[index++]=(int)Math.pow(n,2);
+                } else{
+                    dart[index++]=(int)Math.pow(n,3);
+                } 
+                numstr="";
+            } else {
+                if(c=='*'){
+                    dart[index-1]*=2;
+                    if(index-2>=0) dart[index-2]*=2; 
+                } else {
+                dart[index-1]*=(-1);
+                }
+            }
+        }
+    
+        answer=dart[0]+dart[1]+dart[2];
+    
+        return answer;
+    }
+}
